98c74fb52b196abd986b7d38fd07ddd6c2f49d02
Jocinell <afolagbadejoy8@gmail.com>
2021-11-06 17:04:22 +0100

    initial commit of project

---
+++ project.js
@@ -0,0 +1,12 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+const projectname = "Afolagbade tribute-page";
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
